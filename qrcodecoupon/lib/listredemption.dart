4ccad0c190387c2deebd6fb87dca752e2f707b97
maryamumairah <maryamumairah12@gmail.com>
2024-01-29 20:19:08 +0800

    updated navigation bar listredemption

--- qrcodecoupon/lib/listredemption.dart
+++ qrcodecoupon/lib/listredemption.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:qrcodecoupon/qrscanner.dart';
 import 'coupon.dart';
 
 class CouponList extends StatefulWidget {
@@ -9,13 +10,15 @@ class CouponList extends StatefulWidget {
   State<CouponList> createState() => _CouponListState(); 
 }
 
-class _CouponListState extends State<CouponList> {
+class _CouponListState extends State<CouponList> with SingleTickerProviderStateMixin {
   List<Coupon> coupons = [];
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
     fetchCoupons();
+    _tabController = TabController(length: 3, vsync: this);
   }
 
   Future<void> fetchCoupons() async {
@@ -34,17 +37,56 @@ class _CouponListState extends State<CouponList> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Redeemed Coupons'),
+      bottom: TabBar(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(
+              icon: Icon(Icons.qr_code_scanner_sharp),
+            ),
+            Tab(
+              icon: Icon(Icons.list),
+            ),
+            Tab(
+              icon: Icon(Icons.account_circle_outlined),
+            ),
+          ],
+        ),
       ),
-      body:
-      ListView.builder(
-        itemCount: coupons.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Coupon ID: ${coupons[index].code}'),
-            subtitle: Text('Price: \$${coupons[index].price}', style: const TextStyle(fontSize: 18)),
-            trailing: Text('Expiry Date: ${coupons[index].validity}', style: const TextStyle(fontSize: 18)),
-          );
-        },
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          ListView.builder(
+            itemCount: coupons.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('Coupon ID: ${coupons[index].code}'),
+                subtitle: Text('Price: \$${coupons[index].price}', style: const TextStyle(fontSize: 18)),
+                trailing: Text('Expiry Date: ${coupons[index].validity}', style: const TextStyle(fontSize: 18)),
+              );
+            },
+          ),
+          // Replace these with your actual screens
+          const QRScanner(),
+          const CouponList(),
+          //const UserProfilePage(),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: TabBar(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(
+              icon: Icon(Icons.qr_code_scanner_sharp),
+            ),
+            Tab(
+              icon: Icon(Icons.list),
+            ),
+            Tab(
+              icon: Icon(Icons.account_circle_outlined),
+            ),
+          ],
+        ),
       ),
     );
-  }}
+  }
+}
